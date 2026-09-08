b2f34e76f6a7458e823ca959a016c2e5160cc523
dbarmanda <dbarmand26@gmail.com>
2026-09-08 02:53:57 +0000

    Connect API to PostgreSQL (using pool)

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -1,20 +1,31 @@
-const employees = [
-    {
-        id: 1,
-        name: "John",
-        department: "IT",
-        salary: 60000
-    },
-    {
-        id: 2,
-        name: "Alice",
-        department: "HR",
-        salary: 55000
-    }
-];
+//<---- static demo array data
+// const employees = [ 
+//     {
+//         id: 1, name: "John", department: "IT", salary: 60000
+//     },
+//     {
+//         id: 2, name: "Alice", department: "HR", salary: 55000
+//     }
+// ];
 
-function getEmployees(req, res){
-    res.json(employees);
+// function getEmployees(req, res){
+//     res.json(employees);
+// }
+
+const pool = require("../db/db");
+
+async function getEmployees(req, res){
+    try {
+        const result = await pool.query(
+            "SELECT * FROM employees ORDER BY id"
+        );
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Failed to fetch employees from db."
+        });
+    }
 }
 
 module.exports = {
